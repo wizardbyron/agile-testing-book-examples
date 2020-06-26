c043b0744ad637f66f0dd71e7c10de48dc72f53b
wizardbyron <wizardbyron@icloud.com>
2020-06-26 16:31:49 +0800

    Add cucumber report plugin

--- bdd/account-maven/account/src/test/java/account/RunCucumberTest.java
+++ bdd/account-maven/account/src/test/java/account/RunCucumberTest.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
+@CucumberOptions(plugin = {"pretty","json:target/cucumber-report/cucumber.json"})
 public class RunCucumberTest {
 
 }
