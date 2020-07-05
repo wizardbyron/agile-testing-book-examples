95b3f9cfc43b0435aa8154d7706818583f98ee22
wizardbyron <wizardbyron@icloud.com>
2020-07-05 16:22:55 +0800

    Finish open url step definitions

--- bdd/tmall-bdd/src/test/java/tmall/bdd/StepDefinitions.java
+++ bdd/tmall-bdd/src/test/java/tmall/bdd/StepDefinitions.java
@@ -1,23 +1,26 @@
 package tmall.bdd;
 
 import io.cucumber.java.zh_cn.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 
 public class StepDefinitions {
+    WebDriver driver = new FirefoxDriver();
+
     @假定("我没有打开浏览器")
     public void 我没有打开浏览器() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 
     @当("打开浏览器并输入 http:\\/\\/www.tmall.com 的时候")
     public void 打开浏览器并输入_http_www_tmall_com_的时候() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        this.driver.get("http://www.tmall.com");
     }
+
     @那么("我可以进入天猫首页")
     public void 我可以进入天猫首页() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        assert(this.driver.getTitle()).startsWith("天猫tmall.com");
+        this.driver.quit();
     }
 }
