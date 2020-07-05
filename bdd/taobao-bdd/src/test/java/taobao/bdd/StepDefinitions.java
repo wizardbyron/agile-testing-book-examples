2a5c479f414716f1d7069c74fa07399c5ec45e09
wizardbyron <wizardbyron@icloud.com>
2020-07-05 10:39:58 +0800

    Import firefox webdriver

--- bdd/taobao-bdd/src/test/java/taobao/bdd/StepDefinitions.java
+++ bdd/taobao-bdd/src/test/java/taobao/bdd/StepDefinitions.java
@@ -1,24 +1,27 @@
 package taobao.bdd;
 
 import io.cucumber.java.zh_cn.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import static org.junit.Assert.*;
 
 public class StepDefinitions {
+    WebDriver driver = new FirefoxDriver();
+
     @假定("我没有打开浏览器")
     public void 我没有打开浏览器() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+     //   throw new io.cucumber.java.PendingException();
     }
 
     @当("打开浏览器并输入 http:\\/\\/www.taobao.com 的时候")
     public void 打开浏览器并输入_http_www_taobao_com_的时候() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        this.driver.get("http://www.taobao.com");
     }
     @那么("我可以进入淘宝网首页")
     public void 我可以进入淘宝网首页() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+//        throw new io.cucumber.java.PendingException();
     }
 }
