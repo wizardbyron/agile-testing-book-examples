9bcc48f11c59becb311303e8b6e89c6a0b31e04c
wizardbyron <wizardbyron@icloud.com>
2020-07-05 23:56:07 +0800

    Add search case and step definition.

--- bdd/tmall-bdd/src/test/java/tmall/bdd/StepDefinitions.java
+++ bdd/tmall-bdd/src/test/java/tmall/bdd/StepDefinitions.java
@@ -1,8 +1,12 @@
 package tmall.bdd;
 
 import io.cucumber.java.zh_cn.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.List;
 
 
 public class StepDefinitions {
@@ -34,5 +38,26 @@ public class StepDefinitions {
         this.driver.quit();
     }
 
+    @当("我在搜索框输入{string}")
+    public void 我在搜索框输入(String keyword) {
+        this.driver.findElement(By.id("mq")).sendKeys(keyword);
+    }
 
+    @当("点击\"搜索\"按钮后")
+    public void 点击_搜索_按钮后() {
+        this.driver.findElement(By.cssSelector("div.mallSearch-input.clearfix")).findElement(By.tagName("Button")).click();
+    }
+
+    @那么("我应该在搜索结果页看到含有{string}为名称的商品")
+    public void 我应该在搜索结果页看到含有_为名称的商品(String keyword) {
+        List<WebElement> titles = this.driver.findElements(By.className("productTitle"));
+        Boolean result = false; //默认为失败
+        for (WebElement t : titles) {
+            if (t.getText().contains(keyword)){
+                result = true;
+                break; // 找到一个就算成功
+            }
+        }
+        assert(result);
+    }
 }
