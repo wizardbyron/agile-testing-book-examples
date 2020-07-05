9eded64ab50e26fc1d775fc16c441ad90ac7175f
wizardbyron <wizardbyron@icloud.com>
2020-07-05 16:06:42 +0800

    initial empty step definitions.

--- bdd/tmall-bdd/src/test/java/tmall/bdd/StepDefinitions.java
+++ bdd/tmall-bdd/src/test/java/tmall/bdd/StepDefinitions.java
@@ -1,11 +1,23 @@
 package tmall.bdd;
 
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+import io.cucumber.java.zh_cn.*;
 
-import static org.junit.Assert.*;
 
 public class StepDefinitions {
+    @假定("我没有打开浏览器")
+    public void 我没有打开浏览器() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 
+    @当("打开浏览器并输入 http:\\/\\/www.tmall.com 的时候")
+    public void 打开浏览器并输入_http_www_tmall_com_的时候() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @那么("我可以进入天猫首页")
+    public void 我可以进入天猫首页() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 }
