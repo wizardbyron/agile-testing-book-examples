76eff33398e52763a0395321e05317b0e2524896
wizardbyron <wizardbyron@icloud.com>
2020-07-19 10:26:39 +0800

    Initial ATM service project with Spring initializr

---
+++ microservices/maven/atm-service/src/main/java/com/example/atm/AtmServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.atm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmServiceApplication.class, args);
+	}
+
+}
