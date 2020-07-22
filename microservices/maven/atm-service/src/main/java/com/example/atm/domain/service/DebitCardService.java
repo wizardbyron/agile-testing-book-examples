1c0cee0c3acade62da94851cc3ad87310daeaaa0
wizardbyron <wizardbyron@icloud.com>
2020-07-22 21:39:36 +0800

    Fix contract test

--- microservices/maven/atm-service/src/main/java/com/example/atm/domain/service/DebitCardService.java
+++ microservices/maven/atm-service/src/main/java/com/example/atm/domain/service/DebitCardService.java
@@ -1,7 +1,23 @@
 package com.example.atm.domain.service;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
 public class DebitCardService {
+
     public boolean verifyPIN(Long cardID, Integer pin) {
-        return pin == 123456;
+        RestTemplate template = new RestTemplate();
+        try {
+            String requestURL = String.format("http://localhost:8080/verify_pin/%d/%d", cardID, pin);
+            ResponseEntity<Map> entity = template.getForEntity(requestURL, Map.class);
+            Map body = entity.getBody();
+            return "OK".equals(body.get("result")) && (entity.getStatusCode() == HttpStatus.ACCEPTED);
+        }catch (Exception e){
+            return false;
+        }
     }
+
 }
