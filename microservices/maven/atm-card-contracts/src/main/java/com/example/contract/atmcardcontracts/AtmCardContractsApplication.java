16dd2a2880e7103202e048949132407986c7cb9f
wizardbyron <wizardbyron@icloud.com>
2020-08-23 09:39:40 +0800

    Move contracts to an sepearte module

---
+++ microservices/maven/atm-card-contracts/src/main/java/com/example/contract/atmcardcontracts/AtmCardContractsApplication.java
@@ -0,0 +1,13 @@
+package com.example.contract.atmcardcontracts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmCardContractsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmCardContractsApplication.class, args);
+	}
+
+}
