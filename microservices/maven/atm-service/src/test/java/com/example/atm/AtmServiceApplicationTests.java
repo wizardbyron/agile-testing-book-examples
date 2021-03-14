17015be7ea28d1e697607772688a80f45a62aa50
wizardbyron <wizardbyron@icloud.com>
2021-03-14 21:10:26 +0800

    Merge RunCucumberTest.java to AtmServiceApplicationTests.java

--- microservices/maven/atm-service/src/test/java/com/example/atm/AtmServiceApplicationTests.java
+++ microservices/maven/atm-service/src/test/java/com/example/atm/AtmServiceApplicationTests.java
@@ -1,9 +1,17 @@
 package com.example.atm;
 
+import io.cucumber.junit.platform.engine.Cucumber;
+import io.cucumber.spring.CucumberContextConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Scope;
+
+import static io.cucumber.spring.CucumberTestContext.SCOPE_CUCUMBER_GLUE;
 
 @SpringBootTest
+@Cucumber
+@CucumberContextConfiguration
+@Scope(SCOPE_CUCUMBER_GLUE)
 class AtmServiceApplicationTests {
 
 	@Test
